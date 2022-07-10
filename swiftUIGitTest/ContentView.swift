c69b8eca01cd11674fed5f272b71154bfffd55d5
baegsihun <qorwlgml2003@naver.com>
2022-07-10 12:46:07 +0900

    Initial Commit

---
+++ swiftUIGitTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUIGitTest
+//
+//  Created by 백시훈 on 2022/07/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUIGitTest/swiftUIGitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIGitTestApp.swift
+//  swiftUIGitTest
+//
+//  Created by 백시훈 on 2022/07/10.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIGitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
