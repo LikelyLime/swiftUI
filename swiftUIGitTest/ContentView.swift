317dda110b652438157955fa7a3c3e30ed24f698
baegsihun <qorwlgml2003@naver.com>
2022-07-11 22:07:03 +0900

    Text연습 푸쉬

--- swiftUIGitTest/ContentView.swift
+++ swiftUIGitTest/ContentView.swift
@@ -9,18 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, swiftUI!")
-            .font(.largeTitle)
-            .foregroundColor(.red)
-            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-            .colorInvert()
+        VStack {
+            Text("폰트의 굵기")
+                .font(.title)
+            .fontWeight(.bold)
+            Text("글씨색은 foreground / 배경색은 background")
+                .foregroundColor(.blue)
+                .background(Color.yellow)
+            
+            Text("커스텀 폰트, 볼드체, 이텔릭체, 밑줄, 취소선")
+                .font(.custom("Mellon", size: 16))
+                .bold()
+                .italic()
+                .underline()
+                .strikethrough()
+            Text("라인 수 제한과 \n 텍스트 정렬 기능입니다. \n요건 안보임")
+                .lineLimit(2)
+                .multilineTextAlignment(.leading)
+                .fixedSize()
+            (Text("자간과 기준선").kerning(8) + Text("조정이 쉽게 가능합니다.").baselineOffset(8))
+                .font(.system(size: 16))
+        }
         
+            
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .previewInterfaceOrientation(.portrait)
+            .previewInterfaceOrientation(.landscapeRight)
     }
 }
